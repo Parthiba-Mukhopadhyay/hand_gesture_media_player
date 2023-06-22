aebe17d884103421adba9405ca5509c70db549a0
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-06-22 15:24:38 +0530

    videosetup

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ vid=cv2.VideoCapture(0) # captures the video
 # while loop runs and captures video frame by frame till termination condition is satisfiied
 while True:
   ret,frame = vid.read() # to convert the video in frames. the function returns a tuple that is stored in ret 
+  frame=cv2.flip(frame,1) # flips the frame horizontally
+
   cv2.imshow('Window',frame) # to show the captured frames to the user through user window
 
-  #to check for termination condition, escape button code:27
+  #to check for termination condition, escape [esc] button code:27
   if cv2.waitKey(1) == 27:
     cv2.destroyAllWindows()
     vid.release()
